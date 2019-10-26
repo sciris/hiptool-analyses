392ba489a84f20404b93651cb0899d1c075209a3
cliffckerr <questionably@gmail.com>
2019-10-26 14:10:18 -0700

    updated for new hiptool

--- rapid/check-hierarchy.py
+++ rapid/check-hierarchy.py
@@ -10,38 +10,20 @@ which = {'dalys':0, 'deaths':1, 'prevalence':2}['dalys']
 
 afg = bod['Afghanistan'][which]
 
-unsorted_codes = hp.causedict.values()
-unsorted_names = hp.causedict.keys()
-order = pl.argsort(unsorted_codes)
-codes = [unsorted_codes[i] for i in order]
-names = [unsorted_names[i] for i in order]
+codes = hp.burdeninfo.dict.keys()
+names = hp.burdeninfo.dict.values()
 dalys = pl.array([afg[name] for name in names])
-causeinds = range(len(order))
 
 dd = sc.odict()
-cd = sc.odict()
-for c in causeinds:
-    cd[codes[c]] = names[c]
-    dd[codes[c]] = round(dalys[c])
-
-
-h0 = dd['T']
-h1 = sum(dd[['A','B','C']])
-
-hierarchy = sc.odict()
-for code in codes:
-    hierarchy[code] = []
-    for code2 in codes:
-        relationship = hp.burdenhierarchy(code, code2)
-        if relationship == 'child':
-            hierarchy[code].append(code2)
+for c,code in enumerate(codes):
+    dd[code] = round(dalys[c])
 
 problems = sc.odict()
 for code in codes:
     total = dd[code]
     string = f'{code:10} Total: {total:8.0f}'
-    if hierarchy[code]:
-        children = sum(dd[hierarchy[code]])
+    if hp.burdeninfo.children[code]:
+        children = sum(dd[hp.burdeninfo.children[code]])
         ratio = total/children
         string += f' Children: {children:8.0f} Ratio: {ratio:5.3f}'
         if not sc.approx(ratio, 1, 0.01):
